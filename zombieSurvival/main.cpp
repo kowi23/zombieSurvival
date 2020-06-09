06a9f8d15fc83b25fc080f8a1df16aec7bf8de99
kowi23 <x625134x@gmail.com>
2020-06-09 21:27:18 +0200

    gracz chodzi w kierunku kursora, uczciwy podzial ruchu(zawsze zachowana jest stala predkosc), wykrywanie scian

--- zombieSurvival/main.cpp
+++ zombieSurvival/main.cpp
@@ -3,12 +3,13 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 const float pi = M_PI;
+const int windowHight = 900;
+const int windowWidth = 1400;
 
         class Player : public sf::Sprite {
 public:
     std::string name() { return name_; }
-    int size() { return size_; }
-    double speed(){return speed_;};
+    double getSpeed(){return speed;};
     double max_speed() { return max_speed_; }
 
     Player(sf::Texture &texture_guy){
@@ -20,7 +21,7 @@ public:
     void animate(sf::Vector2i &mousePosition){
         //player angle
         sf::Vector2f playerPosition = this->getPosition();
-        angle = atan((mousePosition.x-playerPosition.x)/(playerPosition.y-mousePosition.y))*180/pi;
+        double angle = atan((mousePosition.x-playerPosition.x)/(playerPosition.y-mousePosition.y))*180/pi;
 
 
         if(mousePosition.y>playerPosition.y){
@@ -40,24 +41,37 @@ public:
 
         //move
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            this->move(0,-4);
+            this->move(std::cos((angle-90)*pi/180)*speed, std::sin((angle-90)*pi/180)*speed);
         }
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-            this->move(0,4);
+            this->move(std::cos((angle+90)*pi/180)*speed, std::sin((angle+90)*pi/180)*speed);
         }
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            this->move(4,0);
+            this->move(std::cos(angle*pi/180)*speed, std::sin(angle*pi/180)*speed);
         }
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            this->move(-4,0);
+            this->move(std::cos((angle-180)*pi/180)*speed, std::sin((angle-180)*pi/180)*speed);
+        }
+        //walls
+        if(playerPosition.x<playerRadius){
+            this->setPosition(playerRadius,playerPosition.y);
+        }
+        if(playerPosition.y<playerRadius){
+            this->setPosition(playerPosition.x,playerRadius);
+        }
+        if(playerPosition.y>windowHight-playerRadius){
+            this->setPosition(playerPosition.x,windowHight-playerRadius);
+        }
+        if(playerPosition.x>windowWidth-playerRadius){
+            this->setPosition(windowWidth-playerRadius,playerPosition.y);
         }
 
     }
 protected:
+    int playerRadius = 13;
     double angle = 0;
     std::string name_;
-    int size_ = 12;
-    double speed_;
+    double speed = 5;
     double max_speed_;
 };
 
@@ -65,7 +79,7 @@ protected:
 
 int main() {
     // create the window
-    sf::RenderWindow window(sf::VideoMode(1400, 900), "My window");
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHight), "My window");
     window.setFramerateLimit(60);
     // create some shapes
     sf::Texture texture_guy;
